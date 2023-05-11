4c09196a28c45bf2dfe0aa1da1ae0c76c3762c02
Gavin Niederman <gavinniederman@gmail.com>
2023-05-11 11:03:22 -0700

    update example

--- src/main.rs
+++ src/main.rs
@@ -3,47 +3,37 @@
 
 use pros::prelude::*;
 
-autonomous!();
-
-on_initialize!();
-
-on_disable!();
-
-comp_init!();
-
-// This code will be run during the teleop period.
-opcontrol! ({
+#[no_mangle]
+pub extern "C" fn opcontrol() {
     // Create a new motor plugged into port zero. The motor will brake when not moving.
     let motor = pros::motor::Motor::new(0, pros::motor::BrakeMode::Brake).unwrap();
     // Create a controller, specifically controller 1.
     let controller = pros::controller::Controller::new(pros::controller::ControllerId::Master);
 
-    // Create a copy of the motor to be moved into the print task.
-    let motor_copy = motor.clone();
-
     // Spawn a new task that will print whether or not the motor is stopped constantly.
-    // Priority changes how much time the operating system spends on the task, this can usually be set to default.
-    // Stack depth is how large the stack that is given to the task is, again it can usually be left at default.
-    // Name is an optional string usefull for debugging.
-    pros::multitasking::Task::spawn(
-        move || loop {
-            println!("Motor stopped? {}", motor_copy.get_state().stopped);
+    pros::multitasking::TaskBuilder::new(
+        {
+            // Clone the motor to be used in the task.
+            let motor = motor.clone();
 
-            // Sleep the task as to not steal processing time from the OS.
-            // This should always be done in any loop, including loops in the main task.
-            pros::multitasking::sleep(core::time::Duration::from_millis(20));
-        },
-        pros::multitasking::TaskPriority::Default,
-        pros::multitasking::TaskStackDepth::Default,
-        Some("Print Task"),
-    );
+            move || loop {
+                println!("Motor stopped? {}", motor.get_state().stopped);
+
+                // Sleep the task as to not steal processing time from the OS.
+                // This should always be done in any loop, including loops in the main task.
+                pros::multitasking::sleep(core::time::Duration::from_millis(20));
+            }
+        }
+    )
+    .name("Print Task")
+    .build();
 
     loop {
         // Set the motors output with how far up or down the right joystick is pushed.
-        // Set raw takes in an i8 which is scaled to -12 to 12 volts.
-        motor.set_raw_output(controller.state().joysticks.right.y);
+        // Set output takes a float from -1 to 1 that is scaled to -12 to 12 volts.
+        motor.set_output(controller.state().joysticks.right.y);
 
         // Once again, sleep.
         pros::multitasking::sleep(core::time::Duration::from_millis(20));
     }
-});
+}
